d4076e24d1728a59ceb10215917ba5679004438a
guxhss <gugaman2101@gmail.com>
2026-04-21 16:03:30 +0100

    Atualização com as respostas sql

--- rsc/sql/init.sql
+++ rsc/sql/init.sql
@@ -42,3 +42,51 @@ INSERT INTO enrollments VALUES
 (1, 2),
 (2, 1),
 (3, 3);
+
+--1--
+SELECT * 
+FROM students;
+
+--2--
+SELECT s.full_name, d.name AS department
+FROM students s
+JOIN departments d ON s.department_id = d.id;
+
+--3--
+SELECT status, COUNT(*) AS total_students
+FROM students
+GROUP BY status;
+
+--4--
+SELECT c.title AS course, d.name AS department
+FROM courses c
+JOIN departments d ON c.department_id = d.id;
+
+--5--
+SELECT s.full_name, c.title AS course, e.final_status
+FROM enrollments e
+JOIN students s ON e.student_id = s.id
+JOIN course_offerings co ON e.offering_id = co.id
+JOIN courses c ON co.course_id = c.id;
+
+--6--
+SELECT co.id AS offering_id, AVG(g.grade) AS avg_grade
+FROM grades g
+JOIN assignments a ON g.assignment_id = a.id
+JOIN course_offerings co ON a.offering_id = co.id
+GROUP BY co.id;
+
+--7--
+SELECT s.full_name
+FROM students s
+LEFT JOIN grades g ON s.id = g.student_id
+WHERE g.id IS NULL;
+
+--8--
+SELECT c.title AS course, COUNT(e.student_id) AS total_students
+FROM enrollments e
+JOIN course_offerings co ON e.offering_id = co.id
+JOIN courses c ON co.course_id = c.id
+GROUP BY c.title
+HAVING COUNT(e.student_id) > 2;
+
